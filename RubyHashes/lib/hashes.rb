505e7dea7d9b0e8ace1b61c730f2ca794b1f3e2e
Kevin Kim <kkim36@uncc.edu>
2019-02-03 17:25:14 +0000

    RubyHashes - Part 2

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -14,18 +14,19 @@ end
 
 # Part II
 def array2d_2_hash contact_info, contacts
+    if contact_info.length == 1
+        return contacts
+    end
     count = 0
     contacts.each do |key, value|
-        contact_info.each do |x|
-            x.each do |cell|
-                person = {:email=>x, :phone=>cell}
-        contacts[key] = person
-            end
-        end
-    count = count + 1
+        contacts[key] = {email:contact_info[count][0], phone:contact_info[count][1]}
+        count += 1
     end
     return contacts
 end
+
+
+
 
 # Part III
 def hash_2_array contacts
