0653fd78cdf834245c790464e13924f5ce90b51c
Kevin Kim <kkim36@uncc.edu>
2019-02-21 14:59:58 +0000

    Ruby OOP all done

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,19 +1,47 @@
 class Dessert
   # add code for setters and getters
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
+    def name=(name)
+      @name = name
+    end
+    def name
+      @name
+    end
+    def calories=(calories)
+      @calories = calories
+    end
+    def calories
+      @calories
+    end
   end
   def healthy?
-    # your code here
+    if(self.calories < 400)
+      return true
+    end
+    return false
   end
   def delicious?
-    # your code here
+    if(name.include?("bean") && flavor == "licorice")
+      return false
+    end
+    return true
   end
 end
 
 class JellyBean < Dessert
   # add code for setters and getters
   def initialize(flavor)
-    # your code here
+    @name = ""
+    @name = flavor + " jelly bean"
+    @calories = 5
+    @flavor = flavor
+    def flavor=(flavor)
+      @flavor = flavor
+    end
+    def flavor
+      @flavor
+    end
   end
 end
