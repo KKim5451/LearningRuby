048cf8cd1267af62fcf3819a9f23bf43e7baa589
Kevin Kim <kkim36@uncc.edu>
2019-02-26 16:02:47 +0000

    Ruby 00 Basics - Part 3

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -7,4 +7,14 @@ class BookInStock attr_accessor :isbn, :price
         raise ArgumentError, 'String is empty' unless @isbn.length > 0
         raise ArgumentError, 'Price is 0 or negative' unless @price > 0.0
     end
+    
+    def price_as_string
+        ret = "S"
+        if(@price < 1.0)
+            ret = "$0.%.2f" %price
+        end
+        if(@price > 1.0)
+            ret = "$%.2f" %price
+        end
+    end
 end
