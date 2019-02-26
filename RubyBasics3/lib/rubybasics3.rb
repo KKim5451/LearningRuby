5c00bc3f1076ac4af4de803542b3552140776cbe
Kevin Kim <kkim36@uncc.edu>
2019-02-26 15:35:05 +0000

    Ruby 00 Basics - Part 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,20 @@
 # Ruby Basics Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    def isbn=(isbn)
+      @isbn = isbn
+    end
+    def isbn
+      @isbn
+    end
+    def price=(price)
+      @price = price
+    end
+    def price
+      @price
+    end
+  end
 end
