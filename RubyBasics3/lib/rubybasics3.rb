529e0f1aa0dc0c2a760780179bc898b463cfd2a4
Kevin Kim <kkim36@uncc.edu>
2019-02-26 15:59:18 +0000

    Ruby 00 Basics - Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,20 +1,10 @@
 # Ruby Basics Part 3
 
-class BookInStock
-  def initialize(isbn, price)
-    @isbn = isbn
-    @price = price
-    def isbn=(isbn)
-      @isbn = isbn
+class BookInStock attr_accessor :isbn, :price
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = price
+        raise ArgumentError, 'String is empty' unless @isbn.length > 0
+        raise ArgumentError, 'Price is 0 or negative' unless @price > 0.0
     end
-    def isbn
-      @isbn
-    end
-    def price=(price)
-      @price = price
-    end
-    def price
-      @price
-    end
-  end
 end
